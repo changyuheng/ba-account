70466fc21e56986e9780b534fa9e6490b4016065
Henry Chang <mr.changyuheng@gmail.com>
2018-10-09 15:20:22 +0800

    Switch to Mozilla Public License 2.0 (MPL-2.0)

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+# Copyright 2015-2018 Henry Chang
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from flask import Flask, redirect, render_template, request
 import subprocess
 app = Flask(__name__)
